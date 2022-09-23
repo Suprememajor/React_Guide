04de896a6ab1069b477b264e833266e41fe015ad
Suprememajor <ndipnobertetta@gmail.com>
2022-09-23 19:15:16 +0100

    refavtored to make use of props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,36 @@ import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      title: "Car Insurance 1",
+      amount: 294.67,
+      date: new Date(2022, 9, 20),
+    },
+    {
+      title: "Car Insurance 2",
+      amount: 294.67,
+      date: new Date(2022, 9, 20),
+    },
+    {
+      title: "Car Insurance 3",
+      amount: 294.67,
+      date: new Date(2022, 9, 20),
+    },
+    {
+      title: "Car Insurance 4",
+      amount: 294.67,
+      date: new Date(2022, 9, 20),
+    },
+  ];
   return (
     <div className="App">
       <h2>Let's get started!</h2>
       <p>This is also visible.</p>
-      <ExpenseItem />
+      <ExpenseItem data={expenses[0]}/>
+      <ExpenseItem data={expenses[1]}/>
+      <ExpenseItem data={expenses[2]}/>
+      <ExpenseItem data={expenses[3]}/>
     </div>
   );
 }
