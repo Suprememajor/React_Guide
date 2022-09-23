5a58127f91b06180c0142d366599e2c871334358
Suprememajor <ndipnobertetta@gmail.com>
2022-09-24 00:32:53 +0100

    added card component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate data={props.data.date}/>
       <div className="expense-item__description">
         <h2>{props.data.title}</h2>
         <div className="expense-item__price">{props.data.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
