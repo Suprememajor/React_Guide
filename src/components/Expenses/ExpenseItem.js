7935786fd3b5594bc22e2e2d2de88caed74b853c
Suprememajor <ndipnobertetta@gmail.com>
2022-09-24 16:25:06 +0100

    added a button and a click listener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,9 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("Clicked");
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate data={props.data.date}/>
@@ -10,6 +13,7 @@ function ExpenseItem(props) {
         <h2>{props.data.title}</h2>
         <div className="expense-item__price">{props.data.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
